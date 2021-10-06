81883acf2255c651ed61816303172a41bf884769
felipe <felipecacezi@gmail.com>
2021-10-06 17:17:53 -0300

    'primeiro_commit'

---
+++ src/data/items.ts
@@ -0,0 +1,13 @@
+import { Item } from "../types/Item";
+
+export const items:Item[] = [
+
+    { date: new Date(2021, 9, 14), category: 'food', title: 'MCDonalds', value: 32.00 },
+    { date: new Date(2021, 9, 15), category: 'food', title: 'Burger King', value: 32.50 },
+    { date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel Apt', value: 32.20 },
+    { date: new Date(2021, 10, 15), category: 'salary', title: 'Salário ACME', value: 4500.00 },
+    { date: new Date(2021, 10, 16), category: 'salary', title: 'Salário ACME2', value: 4500.00 },
+    { date: new Date(2021, 10, 17), category: 'salary', title: 'Salário ACME3', value: 4500.00 }    
+
+
+];
